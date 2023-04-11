2cc2cafb39b32fc5de62c7282dddadb2eabb16c8
mthiagoalves <m.thiago.ales@live.com>
2023-04-12 00:11:09 +0100

    feat(prisma): create model and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CartModule } from './cart/cart.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CartModule],
+  imports: [CartModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
