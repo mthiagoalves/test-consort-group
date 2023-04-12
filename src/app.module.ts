10c29610c94ad24957026cd8b2e910970744dd5b
mthiagoalves <m.thiago.ales@live.com>
2023-04-12 02:05:57 +0100

    feat(product): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CartModule } from './cart/cart.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CartModule, PrismaModule],
+  imports: [CartModule, PrismaModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
