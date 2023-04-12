d4c8a6c66d6686e7ea33c7d4c2625d11d2d5fb30
mthiagoalves <m.thiago.ales@live.com>
2023-04-12 14:12:20 +0100

    feat(user): implement dto & entity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,38 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, Matches, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Name the user',
+    example: 'Thiago Alves'
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Username (is unique)',
+    example: 'mthiagoalves'
+  })
+  username: string;
+
+  @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is very weak',
+  })
+  @ApiProperty({
+    description: 'Password',
+    example: 'Abcd@1234'
+  })
+  password: string;
+
+  @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is very weak',
+  })
+  @ApiProperty({
+    description: 'Password confirmation, this password needs to be equal to Password',
+    example: 'Abcd@1234'
+  })
+  confirmPassword: string;
+}
