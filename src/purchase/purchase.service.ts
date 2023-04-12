7fdcf339877a2f9000bdd3bee990d55fb3a32d3e
mthiagoalves <m.thiago.ales@live.com>
2023-04-12 21:10:05 +0100

    feat(order): implement purchase creation (WIP)

--- src/purchase/purchase.service.ts
+++ src/purchase/purchase.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import { handleError } from 'src/utils/handle-error.utils';
 
 @Injectable()
 export class PurchaseService {
   constructor (private readonly prisma: PrismaService){}
-  
+
   create(createPurchaseDto: CreatePurchaseDto) {
-    return 'This action adds a new purchase';
+    const data: Prisma.PurchaseCreateInput = {
+      user: {
+        connect: {
+          id: createPurchaseDto.userId,
+        },
+      },
+      cart: {
+        connectOrCreate: {
+          create: {
+            number: createPurchaseDto.cartNumber
+          },
+          where: {
+            number: createPurchaseDto.cartNumber
+          }
+        }
+      }
+    };
+
+    this.prisma.purchase.create({
+      data
+    }).catch(handleError)
   }
 
   findAll() {
