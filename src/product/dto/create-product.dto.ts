ce8f67a15dd04179e17e69cafb13b42aca142e34
mthiagoalves <m.thiago.ales@live.com>
2023-04-12 02:44:40 +0100

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateProductDto {
-  id: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Product name',
+    example: 'Elden Ring'
+  })
   name: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2
+  })
+  @ApiProperty({
+    description: 'Product price',
+    example: 299.89
+  })
   price: number;
 
 }
