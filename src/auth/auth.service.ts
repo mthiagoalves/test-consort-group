1de2e5456546064159e4965e297d80886c0283c9
mthiagoalves <m.thiago.ales@live.com>
2023-04-13 00:34:38 +0100

    feat(auth): implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { LoginResponseDto } from './dto/login-response.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService,
+    private readonly jwtSerice: JwtService) {}
+
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+    const {username, password} = loginDto;
+
+    const user = await this.prisma.user.findUnique({
+      where: { username }
+    })
+
+    if(!user) {
+      throw new UnauthorizedException('Username or password invalid!');
+    }
+
+    const isHashValid = await bcrypt.compare(password, user.password);
+
+    if(!isHashValid){
+      throw new UnauthorizedException('Username or password invalid!');
+    }
+
+    delete user.password;
+
     return {
-      token: 'Teste',
-      user: undefined
+      token: this.jwtSerice.sign({
+        username
+      }),
+      user
     }
   }
 }
--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,7 +4,7 @@ import { User } from "src/user/entities/user.entity";
 export class LoginResponseDto {
   @ApiProperty({
     description: 'JWT created for login',
-    example: 'TOKEN_CREATED'
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im10aGlhZ29hbHZlcyIsImlhdCI6MTY4MTM0MjQyNCwiZXhwIjoxNjgxMzg1NjI0fQ.t5vEiY0OcKAKHY_QpWuGLVEg1W4vJDw9spaco6cfTFg'
   })
   token: string;
 
